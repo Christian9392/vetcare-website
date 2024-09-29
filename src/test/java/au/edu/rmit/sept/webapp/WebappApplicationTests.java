9b3ddc90fbd57fe55c81be8664b892c68ba583ad
SeanghaiHeng <s4060921@student.rmit.edu.au>
2024-09-29 23:09:09 +1000

    comment Selenium web browsers test (temporary)

--- src/test/java/au/edu/rmit/sept/webapp/WebappApplicationTests.java
+++ src/test/java/au/edu/rmit/sept/webapp/WebappApplicationTests.java
@@ -14,43 +14,43 @@ import java.util.Map;
 @SpringBootTest
 class WebappApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-	@Test
-	void emulateLargeScreen() {
-		WebDriver driver = new ChromeDriver();
-
-		// Change screen size to desktop 1080p
-		driver.manage().window().setSize(new Dimension(1920, 1080));
-
-		driver.get("http://localhost:8080");
-	}
-
-	@Test
-	void emulateMobileScreen() {
-		// Set mobile emulation
-		Map<String, String> mobileEmulation = new HashMap<>();
-		mobileEmulation.put("deviceName", "Nexus 5");
-
-		// Apply options to new web driver
-		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
-		WebDriver driverMobile = new ChromeDriver(chromeOptions);
-
-		driverMobile.get("http://localhost:8080");
-	}
-
-	@Test
-	void seleniumBrowsers() {
-		// Create webdrivers for main browsers
-		WebDriver driverChrome = new ChromeDriver();
-		WebDriver driverFirefox = new FirefoxDriver();
-		WebDriver driverEdge = new EdgeDriver();
-
-		driverChrome.get("http://localhost:8080");
-		driverFirefox.get("http://localhost:8080");
-		driverEdge.get("http://localhost:8080");
-	}
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//	@Test
+//	void emulateLargeScreen() {
+//		WebDriver driver = new ChromeDriver();
+//
+//		// Change screen size to desktop 1080p
+//		driver.manage().window().setSize(new Dimension(1920, 1080));
+//
+//		driver.get("http://localhost:8080");
+//	}
+//
+//	@Test
+//	void emulateMobileScreen() {
+//		// Set mobile emulation
+//		Map<String, String> mobileEmulation = new HashMap<>();
+//		mobileEmulation.put("deviceName", "Nexus 5");
+//
+//		// Apply options to new web driver
+//		ChromeOptions chromeOptions = new ChromeOptions();
+//		chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+//		WebDriver driverMobile = new ChromeDriver(chromeOptions);
+//
+//		driverMobile.get("http://localhost:8080");
+//	}
+//
+//	@Test
+//	void seleniumBrowsers() {
+//		// Create webdrivers for main browsers
+//		WebDriver driverChrome = new ChromeDriver();
+//		WebDriver driverFirefox = new FirefoxDriver();
+//		WebDriver driverEdge = new EdgeDriver();
+//
+//		driverChrome.get("http://localhost:8080");
+//		driverFirefox.get("http://localhost:8080");
+//		driverEdge.get("http://localhost:8080");
+//	}
  }
