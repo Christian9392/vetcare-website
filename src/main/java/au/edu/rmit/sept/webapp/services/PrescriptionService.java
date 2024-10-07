60c476c644a5f108427bb4b0e9cc85c247c7b4ad
WilliamDash <s3947523@student.rmit.edu.au>
2024-10-07 22:27:24 +1100

    feat: #31

--- src/main/java/au/edu/rmit/sept/webapp/services/PrescriptionService.java
+++ src/main/java/au/edu/rmit/sept/webapp/services/PrescriptionService.java
@@ -1,7 +1,7 @@
 package au.edu.rmit.sept.webapp.services;
 
 public interface PrescriptionService {
-    public boolean checkPrescription(Long PrescriptionID);
+    public String checkPrescription(Long PrescriptionID);
     public void decrementPrescription(Long PrescriptionID);
     public void orderPrescription(Long prescriptionID, Long petId);
 }
