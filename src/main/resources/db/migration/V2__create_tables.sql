c1ceea791b2287eb704493a2b5e3db661304cce2
Paul Johny Mampilly <s3968971@student.rmit.edu.au>
2024-10-11 15:23:04 +1100

    V6,V7-edit

--- src/main/resources/db/migration/V2__create_tables.sql
+++ src/main/resources/db/migration/V2__create_tables.sql
@@ -149,16 +149,4 @@ CREATE TABLE `medical_history` (
   FOREIGN KEY (`pet_id`) REFERENCES `pet`(`pet_id`)
 );
 
-CREATE TABLE `dosage` (
-  `dosage_id` BIGINT AUTO_INCREMENT,
-  `date_administered` DATE,
-  `dosage_quantity` VARCHAR(50),
-  `instructions` TEXT,
-  `next_dosage_date` DATE,
-  `medicine_id` BIGINT,
-  `pet_id` BIGINT,
-  `notes` TEXT,
-  PRIMARY KEY (`dosage_id`),
-  FOREIGN KEY (`medicine_id`) REFERENCES `medicine`(`medicine_id`),
-  FOREIGN KEY (`pet_id`) REFERENCES `pet`(`pet_id`)
-);
+
--- src/main/resources/db/migration/V3__insert_into_tables.sql
+++ src/main/resources/db/migration/V3__insert_into_tables.sql
@@ -37,9 +37,4 @@ INSERT INTO medicine (medicine_id, name, description, strength, side_effects, co
 
 
 INSERT INTO prescription (prescription_id, pet_id, vet_id, medicine_id, instructions, dosage_quantity, date_administered, expiry_date, repeats_left, renewal_date) VALUES
-(1, 6, 1, 1, 'testInstuctions', 'testdosage', '2024-10-2', '2024-12-8', 5, '2024-10-10'),
-(2, 6, 1, 2, 'testInstuctionsExpired', 'testdosage', '2024-10-2', '2024-1-1', 5, '2024-10-10');
-
-INSERT INTO dosage (dosage_id, date_administered, dosage_quantity, instructions, next_dosage_date, medicine_id, pet_id, notes) VALUES 
-(1, '2024-10-02', 'testdosage', 'testInstuctions', '2025-02-06', 1, 6, 'none'),
-(2, '2024-10-02', 'testdosage', 'testInstuctionsExpired', '2025-03-07', 2, 6, 'none');
+(1, 6, 1, 1, 'testInstuctions', 'testdosage', '2024-10-2', '2024-12-8', 5, '2024-10-10');
