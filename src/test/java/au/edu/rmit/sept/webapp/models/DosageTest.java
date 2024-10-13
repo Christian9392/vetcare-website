184a10fd3463cda7a6d4b3bf67d6dba3b5b9c766
Paul Johny Mampilly <s3968971@student.rmit.edu.au>
2024-10-13 17:18:15 +1100

    DosageTest/TC1

---
+++ src/test/java/au/edu/rmit/sept/webapp/models/DosageTest.java
@@ -0,0 +1,34 @@
+package au.edu.rmit.sept.webapp.models;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Date;
+
+class DosageTest {
+
+    // Test Case 1: Verify object creation and field access
+    @Test
+    void testDosageCreation() {
+        Pet pet = new Pet();
+        Medicine medicine = new Medicine();
+        String dosageQuantity = "5ml";
+        String instructions = "Twice a day";
+        Date dateAdministered = new Date();
+        Date nextDosageDate = new Date();
+        Dosage dosage = new Dosage();
+        dosage.setPet(pet);
+        dosage.setMedicine(medicine);
+        dosage.setDosageQuantity(dosageQuantity);
+        dosage.setInstructions(instructions);
+        dosage.setDateAdministered(dateAdministered);
+        dosage.setNextDosageDate(nextDosageDate);
+        assertEquals(pet, dosage.getPet());
+        assertEquals(medicine, dosage.getMedicine());
+        assertEquals(dosageQuantity, dosage.getDosageQuantity());
+        assertEquals(instructions, dosage.getInstructions());
+        assertEquals(dateAdministered, dosage.getDateAdministered());
+        assertEquals(nextDosageDate, dosage.getNextDosageDate());
+    }
+
+}
